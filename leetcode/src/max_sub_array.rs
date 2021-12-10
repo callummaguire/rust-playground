dfd52949b29e6fe1f47304b829e394bf7512d8d7
Callum Maguire <callummaguire1994@gmail.com>
2021-12-10 20:40:23 +0100

    add in leetcode answer

---
+++ leetcode/src/max_sub_array.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-subarray/
+pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    let (mut cur, mut max) = (nums[0], nums[0]);
+    for i in 1..nums.len() {
+        cur = match cur < 0 {
+            true => nums[i],
+            _ => cur + nums[i]
+        };
+        max = max.max(cur);
+    }
+    max
+}
