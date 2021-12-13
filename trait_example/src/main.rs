d10ab31b9d7fa98b55a42e70947997375715cba9
Callum Maguire <callummaguire1994@gmail.com>
2021-12-13 20:39:14 +0100

    add in example of implementing a operator

--- trait_example/src/main.rs
+++ trait_example/src/main.rs
@@ -1,6 +1,6 @@
+use std::ops::{Add};
 
 trait Animal {
-    fn create(name: &'static str) -> self;
     fn name(&self) -> &'static str;
     fn talk(&self) {
         println!("{} cannot talk", self.name());
@@ -21,10 +21,41 @@ impl Animal for Human {
     }
 }
 
+#[derive(Debug)]
+struct Complex<T> {
+    re: T,
+    im: T
+}
+
+
+impl<T> Complex<T> {
+    fn new(re: T, im: T) -> Complex<T> {
+        Complex::<T> {re, im}
+    }    
+}
+
+impl Add for Complex<i32> {
+    type Output = Complex<i32>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im
+        }
+    }
+}
+
+
+
 fn main() {
 
     let me = Human{name: "Callum"};
     
     me.talk();
-    println!("Hello, world! {}", me.name());
+
+    let mut a = Complex::new(1,2);
+    let mut b = Complex::new(3,4);
+
+
+    println!("{:?}", a + b);
 }
