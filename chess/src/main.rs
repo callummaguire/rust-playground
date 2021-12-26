21605f2f2cc9004dc35e515dfb1c846057b97136
Callum Maguire <callummaguire1994@gmail.com>
2021-12-26 19:01:25 +0100

    add movement to the chess board

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,6 +1,27 @@
 mod board;
+use std::io::stdin;
+
 fn main() {
 
     board::start_board_state();
+
+    println!("please make a move");
+    let mut buffer = String::new();
+    match stdin().read_line(&mut buffer) {
+        Ok(_) => {
+            let parsed = buffer.trim_end().parse::<String>();
+            match parsed {
+                Ok(player_move) => {
+                    println!("move {}" , player_move);
+                }
+                Err(e) => {
+                    println!("cound not read your input {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("{}", e)
+        }
+    }
     println!("Hello, world!");
 }
